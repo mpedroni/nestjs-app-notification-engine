0382acdc0a961e1fa6c5301ad8c9bca10e3c47ac
Matheus Pedroni <matheus.pedroni@edu.itajai.sc.gov.br>
2021-05-12 15:40:29 -0300

    aula 15

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('notification-email')
+  sendEmail(@Payload() data: any): void {
+    console.log(data.value);
+  }
+
+  @MessagePattern('notification-phone')
+  sendPhone(@Payload() data: any): void {
+    console.log(data.value);
   }
 }
